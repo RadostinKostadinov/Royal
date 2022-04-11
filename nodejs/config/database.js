1984edd39a8de91a61c2cc4b1e882865c729d77b
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2022-04-12 02:04:52 +0300

    a

--- nodejs/config/database.js
+++ nodejs/config/database.js
@@ -2,8 +2,19 @@ import mongoose from "mongoose";
 
 async function mongoConfig() {
     // Initialize Mongoose
-    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/royal")
+
+    //TODO Да видя как да си направя dev/prod .env file (за dev tiq user,pass,authsource ne trqbva da gi ima)
+    //https://stackoverflow.com/questions/10694571/verify-if-my-node-js-instance-is-dev-or-production
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/royal", {
+        user: process.env.MONGO_USER,
+        pass: process.env.MONGO_PASS,
+        authSource: "admin",
+        useNewUrlParser: true,
+    })
         .then(console.log('Connected to MongoDB at ' + (process.env.MONGO_URI || "mongodb://127.0.0.1:27017/royal")));
+    /* await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/royal")
+        .then(console.log('Connected to MongoDB at ' + (process.env.MONGO_URI || "mongodb://127.0.0.1:27017/royal"))); */
+
 }
 
 export { mongoConfig };
