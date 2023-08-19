039d2d0f06ba744c127ac748a4103bff158b143b
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2023-08-19 20:24:58 +0300

    modified some css

--- web-app/src/app.js
+++ web-app/src/app.js
@@ -3,7 +3,7 @@ import page from 'page';
 import './bootstrap/bootstrap.dark.min.css';
 import './bootstrap/bootstrap.bundle.min.js';
 import './css/global.css';
-import { checkLogin, user } from './views/api/api';
+import { checkLogin } from './views/api/api';
 import { adminPages } from './views/admin/admin';
 import { waiterPages } from './views/waiter/waiter';
 import { bartenderPages } from './views/bartender/bartender';
